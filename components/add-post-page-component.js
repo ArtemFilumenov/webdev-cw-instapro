47da6b7b6058040299e80fefdd996d0d796cc15e
Artem <101066894+ArtemFilumenov@users.noreply.github.com>
2024-04-04 17:22:44 +0300

    fix code "add photo"

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,15 +1,39 @@
+import { renderUploadImageComponent } from "./upload-image-component.js";
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
+    let imageUrl = "";
     // TODO: Реализовать страницу добавления поста
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
+      <div class="form">
+        <h3 class="form-title">Добавить пост</h3>
+        <div class="form-inputs">
+          <div class="upload-image-container">
+          </div>
+        </div>
+        <p>Опишите фотографию:</p>
+        <textarea></textarea>
+        <button class="button" id="add-button">Добавить</button>
+      </div>
     </div>
   `;
 
+  
     appEl.innerHTML = appHtml;
+
+    const uploadImageContainer = appEl.querySelector(".upload-image-container");
+
+    if (uploadImageContainer) {
+      renderUploadImageComponent({
+        element: appEl.querySelector(".upload-image-container"),
+        onImageUrlChange(newImageUrl) {
+          imageUrl = newImageUrl;
+        },
+      });
+    }
+
 
     document.getElementById("add-button").addEventListener("click", () => {
       onAddPostClick({
